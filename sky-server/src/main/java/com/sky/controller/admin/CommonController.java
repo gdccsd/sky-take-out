4aa40dacf1083fd82c2d135a5ab7f7fcec271773
zys <a1522495356@126.com>
2026-03-18 17:13:54 +0800

    菜品分页查询业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -10,13 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.UUID;
 
 @RestController
 @Slf4j
 @RequestMapping("/admin/common")
-public class common {
+public class CommonController {
 
     @Autowired
     private AliOssUtil aliOssUtil;
