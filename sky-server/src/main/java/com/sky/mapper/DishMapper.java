96ff8cb414315cac5b50014e20f97ff6320beec8
zys <a1522495356@126.com>
2026-03-19 16:01:00 +0800

    根据类型id查询菜品业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -76,6 +76,6 @@ public interface DishMapper {
      * @param categoryId
      * @return
      */
-    @Select("select id, name, category_id, price, image, description, status, create_time, update_time, create_user, update_user from dish where category_id = #{categoryId}")
+    @Select("select id, name, category_id, price, image, description, status, create_time, update_time, create_user, update_user from dish where category_id = #{categoryId} order by create_time desc")
     List<Dish> getByCategoryId(Long categoryId);
 }
